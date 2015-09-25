b07f8c3e2da559b5763e228dc4c3556f394c7ce0
Andreas Kruger <ak@krugercorp.dk>
2015-09-25 18:19:12 +0200

    Lets try this

--- mysqld.sh
+++ mysqld.sh
@@ -26,29 +26,6 @@ if [ $? != 0 ]; then
   fi
 fi
 
-# Get the GTID possition
-
-echo  "${LOG_MESSAGE} Get the GTID positon"
-tmpfile=$(mktemp)
-$MYSQLD --wsrep-recover 2>${tmpfile}
-if [ $? != 0 ]; then
-  echo "${LOG_MESSAGE} An error happened while trying to '--wsrep-recover'"
-  cat ${tmpfile}
-  rm  ${tmpfile}
-  exit 1
-fi
-
-wsrep_start_position=$(sed -n 's/.*Recovered\ position:\s*//p' ${tmpfile})
-
-# What should we do if there is no recoverd position?
-# We will not start, as most likely Galera is not configured
-
-if test -z ${wsrep_start_position}
-  then echo "${LOG_MESSAGE} We found no wsrep position!"
-       echo "${LOG_MESSAGE} Most likely Galera is not configured, so we refuse to start"
-       exit 1
-fi
-
 # Start mysqld
 
 exec $MYSQLD $OPT --wsrep_start_position=$wsrep_start_position
@@ -57,4 +34,3 @@ exec $MYSQLD $OPT --wsrep_start_position=$wsrep_start_position
 
 echo "${LOG_MESSAGE} Uhh thats evil! How are you able to see this in your log?!"
 exit 1
-
